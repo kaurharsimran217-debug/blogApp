910dad6574ddcdbea3450e2c31c7a47fe3af7fd4
Harsimran Kaur <kaurharsimran217@gmail.com>
2026-04-03 11:09:29 +0530

    updated_allowed_hosts

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-fd)(4_18&7xu@l1tm%zo=^-5n%o95vpze*y_p^t#oje1ez5_7_'
 SECRET_KEY = os.environ.get('SECRET_KEY')
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
